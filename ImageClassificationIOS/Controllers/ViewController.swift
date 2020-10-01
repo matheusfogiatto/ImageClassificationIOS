93a154e8f492262d2ba619c535aac0e57aa2f81c
Matheus <matheus.fogiattos@gmail.com>
2020-10-01 18:19:09 -0300

    Added TensorFlow Default CameraFeedManagerDelegate / ModelData / CVPixelBufferExtension

--- ImageClassificationIOS/Controllers/ViewController.swift
+++ ImageClassificationIOS/Controllers/ViewController.swift
@@ -7,14 +7,87 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+// MARK: CameraFeedManagerDelegate Methods
+extension ViewController: CameraFeedManagerDelegate {
+    
+    func didOutput(pixelBuffer: CVPixelBuffer) {
+        let currentTimeMs = Date().timeIntervalSince1970 * 1000
+        guard (currentTimeMs - previousInferenceTimeMs) >= delayBetweenInferencesMs else { return }
+        previousInferenceTimeMs = currentTimeMs
+        
+        // Pass the pixel buffer to TensorFlow Lite to perform inference.
+        result = modelDataHandler?.runModel(onFrame: pixelBuffer)
+        
+        // Display results by handing off to the InferenceViewController.
+        DispatchQueue.main.async {
+            let resolution = CGSize(width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            print(result)
+            print(resolution)
+//            self.inferenceViewController?.inferenceResult = self.result
+//            self.inferenceViewController?.resolution = resolution
+//            self.inferenceViewController?.tableView.reloadData()
+        }
+    }
+    
+    // MARK: Session Handling Alerts
+    func sessionWasInterrupted(canResumeManually resumeManually: Bool) {
+        
+        // Updates the UI when session is interupted.
+        if resumeManually {
+            self.resumeButton.isHidden = false
+        } else {
+            self.cameraUnavailableLabel.isHidden = false
+        }
+    }
+    
+    func sessionInterruptionEnded() {
+        // Updates UI once session interruption has ended.
+        if !self.cameraUnavailableLabel.isHidden {
+            self.cameraUnavailableLabel.isHidden = true
+        }
+        
+        if !self.resumeButton.isHidden {
+            self.resumeButton.isHidden = true
+        }
+    }
+    
+    func sessionRunTimeErrorOccured() {
+        // Handles session run time error by updating the UI and providing a button if session can be manually resumed.
+        self.resumeButton.isHidden = false
+        previewView.shouldUseClipboardImage = true
+    }
+    
+    func presentCameraPermissionsDeniedAlert() {
+        let alertController = UIAlertController(title: "Camera Permissions Denied", message: "Camera permissions have been denied for this app. You can change this by going to Settings", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (action) in
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(settingsAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+        previewView.shouldUseClipboardImage = true
+    }
+    
+    func presentVideoConfigurationErrorAlert() {
+        let alert = UIAlertController(title: "Camera Configuration Failed", message: "There was an error while configuring camera.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alert, animated: true)
+        previewView.shouldUseClipboardImage = true
+    }
 }
 
