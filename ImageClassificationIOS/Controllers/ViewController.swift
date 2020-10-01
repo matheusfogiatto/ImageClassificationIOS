6b7b414f75a907865634ea883b552e9719272107
Matheus <matheus.fogiattos@gmail.com>
2020-10-01 18:30:24 -0300

    Fixed some Warkings beacuse of doing project without InterfaceResultsVC

--- ImageClassificationIOS/Controllers/ViewController.swift
+++ ImageClassificationIOS/Controllers/ViewController.swift
@@ -9,18 +9,55 @@
 import UIKit
 import AVFoundation
 
-
-
-
-
-
 class ViewController: UIViewController {
     
     @IBOutlet weak var previewView: PreviewView!
     
+    // MARK: Constants
+    private let animationDuration = 0.5
+    private let collapseTransitionThreshold: CGFloat = -40.0
+    private let expandThransitionThreshold: CGFloat = 40.0
+    private let delayBetweenInferencesMs: Double = 1000
+    
+    // MARK: Instance Variables
+    // Holds the results at any time
+    private var result: Result?
+    private var initialBottomSpace: CGFloat = 0.0
+    private var previousInferenceTimeMs: TimeInterval = Date.distantPast.timeIntervalSince1970 * 1000
+    
+    // MARK: Controllers that manage functionality
+    // Handles all the camera related functionality
+    private lazy var cameraCapture = CameraFeedManager(previewView: previewView)
+    
+    // Handles all data preprocessing and makes calls to run inference through the `Interpreter`.
+    private var modelDataHandler: ModelDataHandler? =
+        ModelDataHandler(modelFileInfo: MobileNet.modelInfo, labelsFileInfo: MobileNet.labelsInfo)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cameraCapture.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cameraCapture.stopSession()
+    }
+    
+    @objc func classifyPasteboardImage() {
+        guard let image = UIPasteboard.general.images?.first else {
+            return
+        }
+        
+        guard let buffer = CVImageBuffer.buffer(from: image) else {
+            return
+        }
+        
+        previewView.image = image
+        
+        DispatchQueue.global().async {
+            self.didOutput(pixelBuffer: buffer)
+        }
     }
 }
 
@@ -38,11 +75,11 @@ extension ViewController: CameraFeedManagerDelegate {
         // Display results by handing off to the InferenceViewController.
         DispatchQueue.main.async {
             let resolution = CGSize(width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
-            print(result)
+            print(self.result ?? "")
             print(resolution)
-//            self.inferenceViewController?.inferenceResult = self.result
-//            self.inferenceViewController?.resolution = resolution
-//            self.inferenceViewController?.tableView.reloadData()
+            //            self.inferenceViewController?.inferenceResult = self.result
+            //            self.inferenceViewController?.resolution = resolution
+            //            self.inferenceViewController?.tableView.reloadData()
         }
     }
     
@@ -51,7 +88,7 @@ extension ViewController: CameraFeedManagerDelegate {
         
         // Updates the UI when session is interupted.
         if resumeManually {
-           // Set resume button hidden to false self.resumeButton.isHidden = false
+            // Set resume button hidden to false self.resumeButton.isHidden = false
         } else {
             // Set resume button hidden to Hidden
             //self.cameraUnavailableLabel.isHidden = false
@@ -60,19 +97,19 @@ extension ViewController: CameraFeedManagerDelegate {
     
     func sessionInterruptionEnded() {
         // Updates UI once session interruption has ended.
-//        if !self.cameraUnavailableLabel.isHidden {
-//            self.cameraUnavailableLabel.isHidden = true
-//        }
-//
-//        if !self.resumeButton.isHidden {
-//            self.resumeButton.isHidden = true
-//        }
+        //        if !self.cameraUnavailableLabel.isHidden {
+        //            self.cameraUnavailableLabel.isHidden = true
+        //        }
+        //
+        //        if !self.resumeButton.isHidden {
+        //            self.resumeButton.isHidden = true
+        //        }
     }
     
     func sessionRunTimeErrorOccured() {
         // Handles session run time error by updating the UI and providing a button if session can be manually resumed.
-//        self.resumeButton.isHidden = false
-//        previewView.shouldUseClipboardImage = true
+        //        self.resumeButton.isHidden = false
+        //        previewView.shouldUseClipboardImage = true
     }
     
     func presentCameraPermissionsDeniedAlert() {
